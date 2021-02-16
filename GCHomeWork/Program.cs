45d79a6bccc38e440eed6ea89b7088aa2c0a7622
Bolsyn <Serikagis@yandex.ru>
2021-02-16 11:52:52 +0600

    Add project files.

---
+++ GCHomeWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace GCHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.С помощью рефлексии получить список методов класса Console и вывести на экран.
+            Type tp = typeof(Console); 
+                        
+            MethodInfo[] methods = tp.GetMethods();
+
+            int i = 0;
+            foreach (MethodInfo mi in methods)
+            {     
+                i++;           
+                Console.WriteLine($"Method[{i}] = {mi.Name}" );
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            // 2.Описать класс с несколькими свойствами. 
+            // Создать экземпляр класса и инициализировать его свойства.
+            // С помощью рефлексии получить свойства и их значения из созданного экземпляра класса. 
+            // Вывести полученные значения на экран
+
+            var testRefl = new TestRefl { Id = 1, Name = "Money", UniqeuProp = "Big" };
+
+            PropertyInfo[] propsOfTest = testRefl.GetType().GetProperties();
+
+            int j = 0;
+            foreach (PropertyInfo mi in propsOfTest)
+            { 
+                j++;
+                Console.WriteLine($"Property[{j}] = {mi.Name}");
+               
+            }
+            foreach (var prop in propsOfTest)
+                if (prop.GetIndexParameters().Length == 0)
+                    Console.WriteLine($"{prop.Name} ({prop.PropertyType.Name}): {prop.GetValue(testRefl, null)}");
+        }
+    }
+}
---
+++ GCHomeWork/TestRefl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCHomeWork
+{
+    public class TestRefl
+    {
+        // 2.Описать класс с несколькими свойствами. 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UniqeuProp { get; set; }
+    }
+}
